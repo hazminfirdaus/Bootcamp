e58bc1b608a3f2df2ea834cdc4ce5c29a0b5dab7
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-05-11 23:23:12 +0800

    fahrenheit-to-celcius-conversion

---
+++ exercises/day11-JavaScript/celcius-to-fahrenheit/index.js
@@ -0,0 +1,6 @@
+'use script';
+
+const temp = prompt('Insert temperature in F here:');
+const insertedTemp = Number(temp.replace(',','.'));  // use .replace to replace , to . //
+const celcius = (insertedTemp - 32) * 5 / 9;
+alert(Math.round(celcius));
