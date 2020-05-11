a7f3cae0c97229bd95a017d2a5171afcd1af744f
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-05-11 22:08:03 +0800

    velocity-conversion

---
+++ exercises/day11-JavaScript/JS Test/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const wage = Number (prompt('Enter your hourly wage:'));
+const gross = 8 * 21 * wage;
+document.write(gross);
---
+++ exercises/day11-JavaScript/velocity-conversion/index.js
@@ -0,0 +1,11 @@
+'use script';
+
+// let mph = 2;
+// const kmh = 1.609334;
+// velocity = 2 * 1.609334;
+// console.log(Math.round(velocity));
+
+const miles = Number(prompt('Insert your velocity in miles here:'));
+const kmh = miles * 1.609334;
+alert(Math.round(kmh));
+
