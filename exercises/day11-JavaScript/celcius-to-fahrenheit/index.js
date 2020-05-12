1ecebae8795a79423c475e2b30cd9c3a87ffa17d
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-05-12 14:13:44 +0800

    update

--- exercises/day11-JavaScript/celcius-to-fahrenheit/index.js
+++ exercises/day11-JavaScript/celcius-to-fahrenheit/index.js
@@ -1,6 +1,6 @@
 'use script';
 
-const temp = (prompt('Insert temperature in F here:'));
-const insertedTemp = Number(temp.replace(',','.'));  // use .replace to replace , to . //
-const celcius = (insertedTemp - 32) * 5 / 9;
-alert(Math.round(celcius));
+let temp = (prompt('Insert temperature in Fahrenheit here:'));
+insertedTemp = Number(temp.replace(',','.'));  // use .replace to replace , to . //
+const celcius = Math.round((insertedTemp - 32) * 5 / 9);
+alert('Temperature in Celcius: ' + celcius);
