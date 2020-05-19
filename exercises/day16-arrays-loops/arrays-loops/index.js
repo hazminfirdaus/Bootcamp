34452028eebf3b8ce95e3e71f03234ee0f361a60
hazminfirdaus <cmhazminfirdaus@gmail.com>
2020-05-19 11:49:53 +0800

    'added'

---
+++ exercises/day16-arrays-loops/arrays-loops/index.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const numbers = [18, 8, -24, 28, -17, 14, -10, 24, 12, -31, 19, 32, -14, -10, 13, 8, -15, -2, 17];
+
+// BASIC ARRAY MANIPULATION
+// 1.
+// console.log(numbers.length);
+
+// 2.
+// console.log(numbers[13]);
+
+// 3.
+// let middle = numbers[Math.floor(numbers.length / 2)];
+// console.log(middle);
+
+
+// BASIC LOOPING
+// 1.
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+
+
+// 2.
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i] ** 2);
+// }
+
+// 3.
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < 0) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+// 4.
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < 0) {
+//     console.log(-numbers[i]);
+//   } else {
+//     console.log(numbers[i]);
+//   }
+// }
+
+// 5.
+// for (let i = 0; i < numbers.length; i +=1) {
+//   if (numbers[i] === -10) {
+//     console.log(i);
+//   }
+// }
+
+// 6.
+// for (let i = 0; i < numbers.length; i +=1) {
+//   if ((numbers[i] % 3) === 0) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+// 7.
+// for (let i = 0; i < numbers.length; i +=1) {
+//   if ((numbers[i] % 2) === 0) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+// 8.
+// for (let i = 0; i < numbers.length; i +=1) {
+//     console.log(5 - numbers[i]);
+// }
+
+// 9.
+// for (let i = 0; i < numbers.length; i +=1) {
+//     console.log((5 - numbers[i]) ** 2);
+// }
+
+// 10. 
+// let negCount = 0;
+// for (let i = 0; i < numbers.length; i +=1) {
+//     if (numbers[i] < 0) {
+//       negCount += 1;
+//     }
+// }
+
+// console.log(negCount);
+
+// 11.
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i +=1) {
+//     sum += numbers[i];
+// }
+
+// console.log(sum / numbers.length);
+
+// 12.
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i +=1) {
+//     if (numbers[i] > 0) {
+//       sum += numbers[i];
+//     }
+// }
+
+// console.log(sum);
+
+
+// INTERMEDIATE LEVEL 
+// 1. 
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > numbers[i-1]) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+// 2. 
+// for (let i = 1; i < numbers.length; i +=1) {
+//   if (numbers[i] > numbers[i-1] && numbers[i] < numbers[i+1]) {
+//     console.log(numbers[i]);
+//   }
+// }
+
+3.
+let sum = 0;
+for (let i = 0; i <numbers.length; i +=1) {
+  sum += numbers[i];
+}
+
+const average = console.log(sum / numbers.length);
+const sumSq = console.log((sum) ** 2);
+console.log(((sum) ** 2) - (sum / numbers.length));
